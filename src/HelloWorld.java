4877ba50c16d57b96d72abfbb273d587b7647c78
jamesfowler <JamesFowlerTX@yahoo.com>
2018-02-27 12:09:19 -0600

    Finished Syntax, Types , and Variables Exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -35,10 +35,64 @@
 //        System.out.println(x);
 //    }
 //}
+//public class HelloWorld {
+//    public static void main(String[] args) {
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//    }
+//}
+
+//public class HelloWorld {
+//    public static void main(String[] args) {
+//        int x = 4;
+//        x = x + 5;
+//        System.out.println(x);
+//    }
+//}
+//
+//public class HelloWorld {
+//    public static void main(String[] args) {
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+//    }
+//}
+
+//public class HelloWorld {
+//    public static void main(String[] args) {
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+//        System.out.println(y);
+//    }
+//}
+
+//public class HelloWorld {
+//    public static void main(String[] args) {
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+//    }
+//}
+
+//public class HelloWorld {
+//    public static void main(String[] args) {
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+//        System.out.println(y);
+//    }
+//}
+
 public class HelloWorld {
     public static void main(String[] args) {
-        int x = 5;
-        System.out.println(++x);
-        System.out.println(x);
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.println(y);
     }
 }
