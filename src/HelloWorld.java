83b7c5bee03de5d490dabac5273f7fef9d7722d2
jamesfowler <JamesFowlerTX@yahoo.com>
2018-02-27 11:43:56 -0600

    java

---
+++ src/HelloWorld.java
@@ -0,0 +1,44 @@
+//public class HelloWorld {
+//    public static void main(String[] args) {
+//        int myFavortieNumber = 3;
+//        System.out.println(myFavortieNumber);
+//    }
+//}
+//public class HelloWorld {
+//    public static void main(String[] args) {
+//        String myString = "Hello from Codeup!";
+//        System.out.println(myString);
+//    }
+//}
+//public class HelloWorld {
+//    public static void main(String[] args) {
+//        char myString = 'C';
+//        System.out.println(myString);
+//    }
+//}
+//public class HelloWorld {
+//    public static void main(String[] args) {
+//        double myString = 3.14159;
+//        System.out.println(myString);
+//    }
+//}
+//public class HelloWorld {
+//    public static void main(String[] args) {
+//        float myNumber = 3.14;
+//        System.out.println(myNumber);
+//    }
+//}
+//public class HelloWorld {
+//    public static void main(String[] args) {
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//    }
+//}
+public class HelloWorld {
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+    }
+}
