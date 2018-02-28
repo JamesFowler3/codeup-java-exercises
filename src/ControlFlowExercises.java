c04b91eddd5a8b52348698a06701d48d95b291e6
jamesfowler <JamesFowlerTX@yahoo.com>
2018-02-28 09:55:24 -0600

    Added Fizz Buzz Exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -21,13 +21,30 @@ public class ControlFlowExercises {
 //
 //
 //            }
-        int i = 2;
-        do {
-            i = i * i;
-            System.out.println(i);
-        }while (i < 1000); {
+//        int i = 2;
+//        do {
+//            i = i * i;
+//            System.out.println(i);
+//        }while (i < 1000); {
+//
+//
+//        }
+        for (int i = 1; i <= 100; i++) {
+            boolean fizzOrBuzz = false;
+            if (i % 3 == 0) {
+                System.out.print("Fizz");
+                fizzOrBuzz = true;
+            }
+            if (i % 5 == 0) {
+                System.out.print("Buzz");
+                fizzOrBuzz = true;
+            }
 
-
+            if (fizzOrBuzz) {
+                System.out.println();
+            } else {
+                System.out.println(String.valueOf(i));
+            }
         }
 
 
