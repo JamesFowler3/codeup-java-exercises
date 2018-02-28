67987767f5ef2ca3b46264b4fcdd231c9e436bf8
jamesfowler <JamesFowlerTX@yahoo.com>
2018-02-28 09:48:48 -0600

    Finished while, and do while loops

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,36 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+//         int i = 5;
+//        while (i <= 15) {
+//            System.out.println("i is " + i);
+//            i++;
+////        }
+//        int i = -2;
+//        do {
+//            i += 2;
+//            System.out.println(i);
+//        }while (i < 100); {
+//
+//
+//            }
+//        int i = 100;
+//        do {
+//            i -= 5;
+//            System.out.println(i);
+//        }while (i > -10); {
+//
+//
+//            }
+        int i = 2;
+        do {
+            i = i * i;
+            System.out.println(i);
+        }while (i < 1000); {
+
+
+        }
+
+
+    };
+    }
+
