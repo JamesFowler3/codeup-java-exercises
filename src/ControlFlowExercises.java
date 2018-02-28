deb883e84bc6bc11fc3b06c6a1809ecb3c15e626
jamesfowler <JamesFowlerTX@yahoo.com>
2018-02-28 10:49:04 -0600

    Worked on table within console

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
 //         int i = 5;
@@ -29,25 +31,41 @@ public class ControlFlowExercises {
 //
 //
 //        }
-        for (int i = 1; i <= 100; i++) {
-            boolean fizzOrBuzz = false;
-            if (i % 3 == 0) {
-                System.out.print("Fizz");
-                fizzOrBuzz = true;
-            }
-            if (i % 5 == 0) {
-                System.out.print("Buzz");
-                fizzOrBuzz = true;
-            }
+//        for (int i = 1; i <= 100; i++) {
+//            boolean fizzOrBuzz = false;
+//            if (i % 3 == 0) {
+//                System.out.print("Fizz");
+//                fizzOrBuzz = true;
+//            }
+//            if (i % 5 == 0) {
+//                System.out.print("Buzz");
+//                fizzOrBuzz = true;
+//            }
+//
+//            if (fizzOrBuzz) {
+//                System.out.println();
+//            } else {
+//                System.out.println(String.valueOf(i));
+//            }
+//        }
 
-            if (fizzOrBuzz) {
-                System.out.println();
-            } else {
-                System.out.println(String.valueOf(i));
-            }
+        String userInput;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("What number would you like to go up to? ");
+        userInput = sc.nextLine();
+        System.out.println("Here is your table!");
+        String leftAlignFormat = "| %-5s | %-5s | %-5s |%n";
+
+        System.out.format("+---------+---------+-------+%n");
+        System.out.format("|  number | squared | cubed |%n");
+        System.out.format("+---------+---------+-------+%n");
+        for (int i = 0; i < 5; i++) {
+            System.out.format(leftAlignFormat, "some data" + i, i * i);
         }
+        System.out.format("+-----------------+------+%n");
 
+    }
 
     };
-    }
+
 
