cb9d8a03939f0984181b906084f021164785f1a4
jamesfowler <JamesFowlerTX@yahoo.com>
2018-02-28 16:20:46 -0600

    Finished Methods Exercise

---
+++ src/MethodsExercises.java
@@ -0,0 +1,18 @@
+public class MethodsExercises {
+    public static void main(String[] args) {
+        System.out.println(division(2, 3));
+    }
+//        public static int addition( int num, int num2){
+//            return (num + num2);
+//        }
+
+//        public static int subtraction (int num, int num2) {
+//        return (num - num2);
+//        }
+//        public static int multiplication (int num, int num2) {
+//        return (num * num2);
+//        }
+        public static double division (double num , double num2) {
+      return (num / num2);
+        }
+    }
